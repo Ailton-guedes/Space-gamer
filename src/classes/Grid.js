824b55790ad012c3690b1e4528a20124c27e842d
ailton guedes <ailton2218@gmail.com>
2025-11-04 16:06:43 -0300

    foi implementando movimentação da nave criardo os inimigos e adicionado a função atirar

---
+++ src/classes/Grid.js
@@ -0,0 +1,90 @@
+import Invader from "./invader.js";
+
+class Grid{
+    constructor(rows, cols) {
+        this.rows = rows;
+        this.cols = cols;
+
+        this.direction = "right";
+        this.moveDown = false;
+
+        this.invaderVelocity = 1;
+        this.invader = this.init();
+
+
+    }
+
+    init() {
+        const array = []
+
+        for (let row = 0; row < this.rows; row +=1){
+
+            for(let col = 0; col < this.cols; col += 1){
+                const invader = new Invader({
+                    x: col * 50 + 20,
+                    y: row * 35 + 20,
+
+                },
+
+                this.invaderVelocity
+            );
+
+            array.push(invader);
+
+            }
+        }
+
+        return array;
+    }
+
+    draw(ctx){
+        this.invader.forEach((invader) => invader.draw(ctx));
+    }
+
+    update() {
+        if (this.chegouNaBordaDireita()){
+            this.direction = "left";
+            this.moveDown = true;
+        } else if (this.chegouNaBordaEsquerda()) {
+            this.direction = "right";
+            this.moveDown = true;
+        }
+
+        this.invader.forEach((invader) => {
+
+            if (this.moveDown) {
+                invader.moveDown();
+                invader.moveDown();
+                invader.incrementVelocity(0.1);
+                this.invaderVelocity = invader.velocity;
+            }
+
+            if (this.direction === "right") invader.moveRigtht();
+            if (this.direction === "left") invader.moveLeft();
+
+        });
+
+        this.moveDown = false;
+
+    }
+    chegouNaBordaDireita(){
+        return this.invader.some(
+            (invader) => invader.position.x + invader.width >= innerWidth
+        );
+    }
+
+ 
+    chegouNaBordaEsquerda(){
+          return this.invader.some(
+            (invader) => invader.position.x <= 0
+        );
+    }
+
+    getRondomInvader(){
+        const index = Math.floor(Math.random() * this.invader.length);
+        return this.invader[index];
+    }
+}
+
+
+export default Grid;
